61f246248570d082637333d1d1a1fd9dced6e53f
paulunder <gameon.padawan@gmail.com>
2024-07-29 21:40:35 +0200

    changed to debug = true

--- seeheimat/settings.py
+++ seeheimat/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$2#va&l8uoiz&n(dxcwcfwqgo#2)a^p0ga=w24n)-l2=jp)rve'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com",]
 
