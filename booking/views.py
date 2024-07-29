673e065665ab70a141dc6c5ee9c784583ac528b3
paulunder <gameon.padawan@gmail.com>
2024-07-29 20:13:16 +0200

    created the url files

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookingForm
 
-# Create your views here.
+def booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('booking')
+    else:
+        form = BookingForm()
+    return render(request, 'booking/booking.html', {'form': form})
