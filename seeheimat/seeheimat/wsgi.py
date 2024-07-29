94a52a9df32a2061d45fdd081633f2706c05d36e
paulunder <gameon.padawan@gmail.com>
2024-07-29 19:32:09 +0200

    added print statement for debugging

--- seeheimat/seeheimat/wsgi.py
+++ seeheimat/seeheimat/wsgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+print("Loading WSGI application...")
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seeheimat.settings')
