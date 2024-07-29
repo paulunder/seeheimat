828e0332f1ff4d37911252624b44275ce6858820
paulunder <gameon.padawan@gmail.com>
2024-07-29 15:24:42 +0200

    added Blog view

--- seeheimat/blog/views.py
+++ seeheimat/blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import BlogPost
+from .forms import CommentForm
 
-# Create your views here.
+def blog_list(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'blog/blog_list.html', {'posts': posts})
+
+def blog_detail(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('blog_detail', post_id=post_id)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/blog_detail.html', {'post': post, 'form': form})
