4d823e0f836f4b23e2690b1035dfb52741a8ca57
paulunder <gameon.padawan@gmail.com>
2024-07-30 20:38:48 +0200

    chagned url

--- booking/urls.py
+++ booking/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import book_service, booking_confirmation
 
 urlpatterns = [
-    path('book/', book_service, name='book_service'),
+    path('book_services/', book_service, name='book_service'),
     path('confirmation/<int:booking_id>/', booking_confirmation, name='booking_confirmation'),
 ]
