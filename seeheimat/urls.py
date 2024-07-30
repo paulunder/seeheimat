c53a2fc88d00fff43b4788ea3718215880d9daaf
paulunder <gameon.padawan@gmail.com>
2024-07-30 21:33:23 +0200

    integrated the signup and the login with allauth

--- seeheimat/urls.py
+++ seeheimat/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path('', include('pages.urls')),
     path('blog/', include('blog.urls')),
     path('booking/', include('booking.urls')),
