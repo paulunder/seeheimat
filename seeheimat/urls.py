dacbd4008f411ead56a5035812662d8cde7a9e04
paulunder <gameon.padawan@gmail.com>
2024-08-02 20:50:29 +0200

    updated urls

--- seeheimat/urls.py
+++ seeheimat/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('', include('pages.urls')),
-    path('blog/', include('blog.urls')),
+    path('summernote/', include('django_summernote.urls')),
     path('booking/', include('booking.urls')),
 ]
