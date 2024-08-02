0d3605c778ec5c7340f5ed2faccaaa6da1a310da
paulunder <gameon.padawan@gmail.com>
2024-08-02 21:19:59 +0200

    updated http reverse proxy

--- blog/views.py
+++ blog/views.py
@@ -77,7 +77,7 @@ def comment_edit(request, slug, comment_id):
         else:
             messages.add_message(request, messages.ERROR, 'Error updating comment!')
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+    return HttpResponseRedirect(reverse('blog_detail', args=[slug]))
 
 
 def comment_delete(request, slug, comment_id):
