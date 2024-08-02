e47b496b9c36ca2a6a16a07ea097feb1dd61ec66
paulunder <gameon.padawan@gmail.com>
2024-08-02 19:58:56 +0200

    added a calendar to booking application

--- booking/forms.py
+++ booking/forms.py
@@ -7,9 +7,13 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['service', 'time']
+        widgets = {
+            'time': forms.TextInput(attrs={'class': 'form-control datetimepicker-input', 'data-target': '#datetimepicker1'})
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
+        self.helper.template = 'bootstrap5'  # Specify the template for Bootstrap 5
         self.helper.add_input(Submit('submit', 'Book Now', css_class='btn-primary btn-block'))
