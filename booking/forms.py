c7a038aeb922e9b88325303620523423e4f66498
paulunder <gameon.padawan@gmail.com>
2024-08-02 19:47:02 +0200

    updated booking

--- booking/forms.py
+++ booking/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Booking
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['service', 'date', 'time']
+        fields = ['service', 'time']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Book Now', css_class='btn-primary btn-block'))
