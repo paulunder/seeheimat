13467a7eb76f92480f297b47d64794d771187faf
paulunder <gameon.padawan@gmail.com>
2024-08-02 20:56:23 +0200

    updatred urls for blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name='home'),
+    path("blog_list", views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
         views.comment_edit, name='comment_edit'),
--- seeheimat/urls.py
+++ seeheimat/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('', include('pages.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('blog/', include('blog.urls')),
     path('booking/', include('booking.urls')),
 ]
