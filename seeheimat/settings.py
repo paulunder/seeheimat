92251b04550d2b7c510583bdc743ebbd847498d5
paul under <gameon.padawan@gmail.com>
2024-08-04 09:26:56 +0000

    bootstrap bugfix try

--- seeheimat/settings.py
+++ seeheimat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-$2#va&l8uoiz&n(dxcwcfwqgo#2)a^p0ga=w24n)-l2=jp)rve
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com",]
+ALLOWED_HOSTS = [".herokuapp.com",".gitpod.io",]
 
 
 # Application definition
