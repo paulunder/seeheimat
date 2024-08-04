903de1d517b092eb5040da51f56c3f6c9892d6f0
paulunder <gameon.padawan@gmail.com>
2024-08-04 13:15:38 +0200

    changed booking forms

--- booking/forms.py
+++ booking/forms.py
@@ -4,7 +4,8 @@ from .models import Booking
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['service']
+        fields = ('service', 'date', 'time_slot')
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'})
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time_slot': forms.Select(attrs={'class': 'form-control'})
         }
