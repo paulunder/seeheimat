e7c19df317c0eb3792eb4b807414e6ec72a8233b
paulunder <gameon.padawan@gmail.com>
2024-08-04 12:57:26 +0200

    made time slot list by hand

--- booking/views.py
+++ booking/views.py
@@ -5,14 +5,16 @@ from pages.models import Service
 from .models import Booking
 from .forms import BookingForm
 
-def generate_time_slots(start_time, end_time, interval_minutes=10):
-    slots = []
-    current_time = datetime.datetime.combine(datetime.date.today(), start_time)
-    end_datetime = datetime.datetime.combine(datetime.date.today(), end_time)
-    while current_time <= end_datetime:
-        slots.append(current_time.time())
-        current_time += datetime.timedelta(minutes=interval_minutes)
-    return slots
+TIME_SLOTS = [
+    "14:00", "14:10", "14:20", "14:30", "14:40", "14:50",
+    "15:00", "15:10", "15:20", "15:30", "15:40", "15:50",
+    "16:00", "16:10", "16:20", "16:30", "16:40", "16:50",
+    "17:00", "17:10", "17:20", "17:30", "17:40", "17:50",
+    "18:00", "18:10", "18:20", "18:30", "18:40", "18:50",
+    "19:00", "19:10", "19:20", "19:30", "19:40", "19:50",
+    "20:00", "20:10", "20:20", "20:30", "20:40", "20:50",
+    "21:00", "21:10", "21:20", "21:30"
+]
 
 @login_required
 def book_service(request):
